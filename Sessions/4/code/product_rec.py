4bcb9b032696e5743a3644e459c39914cc1bc7bd
Yoav Ram <yoavram+github@gmail.com>
2015-09-17 08:34:29 +0300

    all in zip

--- Sessions/4/code/product_rec.py
+++
@@ -1,7 +0,0 @@
-def product_rec(a,b):
-    if b == 1:
-        return a
-    return a + product_rec(a,b-1)
-
-
-    
--- Sessions/4/code/sum_digits_rec.py
+++
@@ -1,8 +0,0 @@
-def sum_digits_rec(n):
-    if n == 0:
-        return 0
-    return n % 10 + sum_digits_rec(n/10)
-
-
-
-    
